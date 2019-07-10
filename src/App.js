aa8c979e295b6b852cc68b6b7cd12b4c596808c2
integra <integra@IMSS-JKR-CPU-14.imsspl.com>
2019-07-10 15:39:09 +0530

    updated

--- src/App.js
+++ src/App.js
@@ -6,13 +6,10 @@ import './App.css';
 export default class App extends React.Component {
 
   render() {
-    // const { dispatch, currentContacts } = this.props;
 
     return (<div>
-        Test
         <AppRouter></AppRouter>
     </div>)
   };
 }
 
-// export default App;
